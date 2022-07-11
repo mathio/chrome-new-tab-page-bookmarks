0e64540442a7fea8af174e8778d2a3771d1174ff
Matej Lednicky <matej@lednicky.name>
2022-07-11 19:55:04 +0200

    extension

---
+++ README.md
@@ -0,0 +1,19 @@
+# New Tab Page Bookmarks
+
+Displays your bookmarks in multiple columns for quick access from the new tab page.
+
+
+## Description
+
+- does NOT manage your bookmarks, the built-in bookmarks manager for that (CMD+ALT+B / CTRL+SHIFT+O)
+- does NOT sync your bookmarks, Chrome already does that
+- there are NO settings, deal with it
+- just displays your bookmarks in multiple columns for quick access from the new tab page
+- displays favicons for your bookmarks (via Google API)
+
+
+## Permission justification
+
+### bookmarks justification
+
+Extension requires [access to your bookmarks](https://developer.chrome.com/docs/extensions/reference/bookmarks/) to display them on the new tab page. Your bookmarks do not leave your device. It loads favicons via Google API and might disclose domains & subdomains (not full URLs) from your bookmarks to Google.
---
+++ src/index.js
@@ -0,0 +1,39 @@
+const bookmarksBarId = '1'
+
+const iconUrl = (href) => {
+	const { origin } = new URL(href)
+	return `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${origin}&size=16`
+}
+
+const addElm = (parent, tag, content, attributes) => {
+	const elm = document.createElement(tag)
+	if (content) {
+		elm.innerHTML = content
+	}
+	if (attributes) {
+		Object.entries(attributes).forEach(([ key, value ]) => {
+			elm[key] = value
+		})
+	}
+	parent.append(elm)
+	return elm
+}
+
+const renderNode = (root) => ({ title, children, url: href }) => {
+	const li = addElm(root, 'li')
+	
+	if (children) {
+		addElm(li, 'h3', title)
+		const ul = addElm(li, 'ul')
+		children.forEach(renderNode(ul))
+	} else if(href) {
+		const icon = `<img src="${iconUrl(href)}" alt="${title.substring(0, 1)}" />`
+
+		addElm(li, 'a', `${icon} ${title}`, { href })
+	}
+}
+
+chrome.bookmarks.getSubTree(bookmarksBarId, function([ rootNode ]) {
+	const root = document.querySelector('#root')
+	rootNode.children.forEach(renderNode(root))
+})
