60f06dcebdc959820446f4338b0944953ba9abd9
Denys Honsiorovskyi <honsiorovskyi@gmail.com>
2022-08-03 15:46:41 +0200

    Firefox support + small improvements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-const bookmarksBarId = '1'
-
 const iconUrl = (href) => {
 	const { origin } = new URL(href)
-	return `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${origin}&size=16`
+	return `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${origin}&size=32`
 }
 
 const addElm = (parent, tag, content, attributes) => {
@@ -11,7 +9,7 @@ const addElm = (parent, tag, content, attributes) => {
 		elm.innerHTML = content
 	}
 	if (attributes) {
-		Object.entries(attributes).forEach(([ key, value ]) => {
+		Object.entries(attributes).forEach(([key, value]) => {
 			elm[key] = value
 		})
 	}
@@ -19,21 +17,28 @@ const addElm = (parent, tag, content, attributes) => {
 	return elm
 }
 
-const renderNode = (root) => ({ title, children, url: href }) => {
-	const li = addElm(root, 'li')
-	
-	if (children) {
-		addElm(li, 'h3', title)
-		const ul = addElm(li, 'ul')
-		children.forEach(renderNode(ul))
-	} else if(href) {
-		const icon = `<img src="${iconUrl(href)}" alt="${title.substring(0, 1)}" />`
+const renderNode =
+	(root) =>
+	({ title, children, url: href }) => {
+		const li = addElm(root, 'li')
 
-		addElm(li, 'a', `${icon} ${title}`, { href })
+		if (children) {
+			addElm(li, 'h3', title)
+			const ul = addElm(li, 'ul')
+			children.forEach(renderNode(ul))
+		} else if (href) {
+			const icon = `<img src="${iconUrl(href)}" alt="${title.substring(0, 1)}" />`
+
+			addElm(li, 'a', `${icon} ${title}`, { href })
+		}
 	}
+
+const renderBookmarksBar = (bookmarksBarId) => {
+	chrome.bookmarks.getSubTree(bookmarksBarId, function ([rootNode]) {
+		const root = document.querySelector('#root')
+		rootNode.children.forEach(renderNode(root))
+	})
 }
 
-chrome.bookmarks.getSubTree(bookmarksBarId, function([ rootNode ]) {
-	const root = document.querySelector('#root')
-	rootNode.children.forEach(renderNode(root))
-})
+const bookmarksBarId = !!chrome.app ? '1' : 'toolbar_____'
+renderBookmarksBar(bookmarksBarId)
