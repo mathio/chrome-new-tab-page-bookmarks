8790b4fb2e8fa18f5798f19d875f3f1df771d657
Matej Lednicky <matej@lednicky.name>
2023-07-30 12:17:21 +0200

    Display an image from Unsplash when bookmarks are hidden

--- README.md
+++ README.md
@@ -37,8 +37,6 @@ Manually:
 
 ![Chrome Screenshot](https://raw.githubusercontent.com/mathio/chrome-new-tab-page-bookmarks/main/store/chrome_screenshot_1280x800.png "Chrome Screenshot")
 
-![Firefox Screenshot](https://raw.githubusercontent.com/mathio/chrome-new-tab-page-bookmarks/main/store/firefox_screenshot_1280x800.png "FIrefox Screenshot")
-
 ## Description
 
 - does NOT manage your bookmarks, use the built-in bookmarks manager for that:
@@ -50,15 +48,21 @@ Manually:
 - there are NO settings, deal with it
 - just displays your bookmarks in multiple columns for quick access from the new tab page
 - displays favicons for your bookmarks (via Google API)
+- displays small "show" / "hide" toggle in bottom right corner to hide all your bookmarks (for when you are sharing your screen or someone is looking over your shoulder)
+- when you hide bookmarks [a random photo](https://unsplash.com/documentation#get-a-random-photo) from [wallpaper topic on Unsplash](https://unsplash.com/t/wallpapers?utm_source=chrome-new-tab-page-bookmarks&utm_medium=referral) is displayed once every 15 minutes
 
 
-## Permission justification
+## Privacy and permission justification
 
 Extension requires [permissions to access your bookmarks](https://developer.chrome.com/docs/extensions/reference/bookmarks/) to display them on the new tab page. Your bookmarks do not leave your device. It loads favicons via Google API ([using t2.gstatic.com/faviconV2 like this](https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://github.com&size=16)) and might disclose domains & subdomains (not full URLs) from your bookmarks to Google.
+
+Wallpaper images from Unsplash API are loaded via proxy. All requests are anonymous and rate-limited via IP address hash. Image is loaded directly from Unsplash CDN and is [tracked by Unsplash](https://unsplash.com/documentation#hotlinking). 
 
 
 ## Development
 
-Run `yarn build` to zip the extension for distribution (for Chrome and Firefox).
+Run `yarn dev` to watch for changes and build the extension for Chrome. Load the extension manually from `dist/chrome-extension` in Chrome (see steps above) to preview changes in your browser.
+
+Run `yarn build` to zip the extension for distribution for both Chrome and Firefox.
 
 It can be distributed manually (as zip file) or manually uploaded to Chrome Web Store and Firefox Add-on Developer Hub.
--- src/index.js
+++ src/index.js
@@ -43,22 +43,97 @@ const renderBookmarksBar = (bookmarksBarId) => {
   })
 }
 
+const TOGGLE_KEY_NAME = 'hideBookmarksBar'
+const isBookmarksBarHidden = () =>
+  window.localStorage.getItem(TOGGLE_KEY_NAME) === 'yes'
+
 const renderToggleLink = () => {
+  const root = document.querySelector('#root')
   const linkElm = document.querySelector('a#toggle')
-  const TOGGLE_KEY_NAME = 'hideBookmarksBar'
-  const isHidden = window.localStorage.getItem(TOGGLE_KEY_NAME) === 'yes'
+
+  const isHidden = isBookmarksBarHidden()
   linkElm.innerText = isHidden ? 'show' : 'hide'
-  document.querySelector('#root').style.display = isHidden ? 'none' : 'flex'
+  root.style.display = isHidden ? 'none' : 'flex'
 
   linkElm.onclick = () => {
-    const wasHidden = window.localStorage.getItem(TOGGLE_KEY_NAME) === 'yes'
+    const wasHidden = isBookmarksBarHidden()
     window.localStorage.setItem(TOGGLE_KEY_NAME, wasHidden ? 'no' : 'yes')
     linkElm.innerText = wasHidden ? 'hide' : 'show'
-    document.querySelector('#root').style.display = wasHidden ? 'flex' : 'none'
+    root.style.display = wasHidden ? 'flex' : 'none'
+    initImage()
   }
 }
+
+const IMAGE_KEY_NAME = 'bookmarksBarImage'
+const IMAGE_VALID_MS = 15 * 60 * 1000 // 15 minutes
+const IMAGE_CLASS_NAME = 'image-bg'
+
+const initImage = async () => {
+  if (!isBookmarksBarHidden()) {
+    hideImage()
+  } else {
+    const image = JSON.parse(
+      window.localStorage.getItem(IMAGE_KEY_NAME) || '{}'
+    )
+    if (image && image.created > Date.now() - IMAGE_VALID_MS) {
+      showImage(image)
+    } else {
+      const utmParams =
+        '?utm_source=chrome-new-tab-page-bookmarks&utm_medium=referral'
+
+      const response = await fetch(
+        `https://unsplash.w2.sk/random-wallpaper.php`
+      )
+      const data = await response.json()
+
+      const { urls, user, links } = data || {}
+
+      const imageUrl = urls?.raw
+      const imageLink = `${links?.html}${utmParams}`
+
+      const userFullName = [user?.first_name, user?.last_name]
+        .filter(Boolean)
+        .join(' ')
+      const userLink = `${user?.links?.html}${utmParams}`
+
+      const attribution = `Photo by <a href="${userLink}">${userFullName}</a> on <a href="${imageLink}">Unsplash</a>`
+      window.localStorage.setItem(
+        IMAGE_KEY_NAME,
+        JSON.stringify({
+          url: imageUrl,
+          attribution,
+          created: Date.now(),
+        })
+      )
+      showImage({ url: imageUrl, attribution })
+    }
+  }
+}
+
+const hideImage = () => {
+  document.body.classList.remove(IMAGE_CLASS_NAME)
+  document.body.style.backgroundImage = 'none'
+  document.querySelector('#image-attribution')?.remove()
+}
+
+const showImage = ({ url, attribution }) => {
+  const imageWidth = window.innerWidth ?? 1920
+  const imageHeight = window.innerHeight ?? 1080
+  const pixelRatio = window.devicePixelRatio ?? 1
+  const resizedImageUrl = `${url}&w=${imageWidth}&h=${imageHeight}&fit=crop&dpr=${pixelRatio}`
+
+  document.body.classList.add(IMAGE_CLASS_NAME)
+  document.body.style.backgroundImage = `url('${resizedImageUrl}')`
+
+  const attributionElm = document.createElement('p')
+  attributionElm.id = 'image-attribution'
+  attributionElm.innerHTML = attribution
+  document.body.appendChild(attributionElm)
+}
+
 window.addEventListener('load', () => {
   const bookmarksBarId = !!chrome ? '1' : 'toolbar_____'
   renderBookmarksBar(bookmarksBarId)
   renderToggleLink()
+  initImage()
 })
